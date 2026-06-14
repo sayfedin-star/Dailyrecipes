bd9da55ed05ced6e02992d27a027c7b1a8c9f539
sayfedin-star <sayfedin@gmail.com>
2026-06-14 10:36:34 +0100

    initial commit: Astro recipe site

--- .astro/integrations/_rafters_astro-meta/virtual-site.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "virtual:astro-meta/site" {
-  export interface SiteIdentity {
-    url: string;
-    name: string;
-    description?: string;
-    locale?: string;
-  }
-  export const site: SiteIdentity;
-}
--- .astro/types.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="astro/client" />
-/// <reference path="integrations/_rafters_astro-meta/virtual-site.d.ts" />
-/// <reference path="content.d.ts" />
